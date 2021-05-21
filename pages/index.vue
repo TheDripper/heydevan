b8e2c84cc6b43db1243497b7bfe62c44516737ad
Dead Customer <deadcustomer@Deads-MacBook-Air.local>
2021-05-20 17:54:08 -0700

    ok

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="root" class="flex">
     <div :class="classes"></div>
-    <div id="tiles" class="w-full" data-push="test">
+    <div id="tiles" class="w-full">
       <div
         :id="post.title.rendered"
         v-for="post in posts"
--- store/index.js
+++ store/index.js
@@ -19,12 +19,12 @@ export const actions = {
   //   commit('posts',posts);
   // },
   async nuxtServerInit({ commit }) {
-    // if(process.env.NODE_ENV=='development') {
-    const posts = await this.$axios.$get("/wp-json/wp/v2/pages");
-    commit("posts", posts);
-    // } else {
-    //   commit('posts',pages);
-    // }
+    if (process.env.NODE_ENV == "development") {
+      const posts = await this.$axios.$get("/wp-json/wp/v2/pages");
+      commit("posts", posts);
+    } else {
+      commit("posts", pages);
+    }
     commit("classes", classes);
   }
 };
